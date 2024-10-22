38cb0469d7dd28cb9dfc771c18608bcfd2f365a0
shadowbrent <alornienwerem@outlook.com>
2024-10-22 11:04:38 +0100

    Final Submission

--- nlptextpreprocess.py
+++ nlptextpreprocess.py
@@ -4,7 +4,12 @@ import re
 import nltk
 import spacy
 import string
+from nltk.corpus import stopwords
+
 pd.options.mode.chained_assignment = None
+
+# Download NLTK stopwords if not already downloaded
+nltk.download('stopwords')
 
 # Load the data
 full_df = pd.read_csv("../input/customer-support-on-twitter/twcs/twcs.csv", nrows=5000)
@@ -16,6 +21,10 @@ df["text"] = df["text"].apply(lambda x: re.sub(r'[a-z]', '', x))
 
 # Remove punctuation
 df["text"] = df["text"].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+
+# Remove frequent words (stopwords)
+stop_words = set(stopwords.words('english'))
+df["text"] = df["text"].apply(lambda x: ' '.join([word for word in x.split() if word.lower() not in stop_words]))
 
 # Display the first few rows of the DataFrame
 df.head()
